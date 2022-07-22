6971459d6d5687d4761794f423202bca2c4d1f89
fulmoon <63829420+fulmoon@users.noreply.github.com>
2022-07-22 11:18:50 +0900

    Feat: add get map data function  onMini

--- lib/homework9.dart
+++ lib/homework9.dart
@@ -1,9 +1,28 @@
 import 'package:flutter/material.dart';
-
 import 'etc/ch5_exam_1.dart';
 
-class Homework9 extends StatelessWidget {
+class Homework9 extends StatefulWidget {
   const Homework9({Key? key}) : super(key: key);
+
+  @override
+  State<Homework9> createState() => _Homework9State();
+}
+
+class _Homework9State extends State<Homework9> {
+  String data = "";
+
+  @override
+  void initState() {
+    super.initState();
+
+    initData();
+  }
+
+  Future initData() async {
+    data = await getData();
+
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,20 +30,20 @@ class Homework9 extends StatelessWidget {
       appBar: AppBar(
         title: const Text('FutureBuild 예제'),
       ), //AppBar
-      body: const Center(
-        child: Text(
-          "결과가 나올 부분",
-          style: TextStyle(fontSize: 40),
-        ),
+      body: Column(
+        children: [
+          Text(
+            data,
+            style: const TextStyle(fontSize: 20),
+          ),
+        ],
       ),
     );
   }
 
-  Future<Map<String, dynamic>> getData() async {
+  Future<String> getData() async {
     await Future.delayed(const Duration(seconds: 3));
 
-    Map<String, dynamic> data = exam1;
-
-    return data;
+    return exam1.toString();
   }
 }
