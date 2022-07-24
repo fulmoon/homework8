fe000c3f0969ee7dc21d7e1c8d0ee9db0b6cd689
fulmoon <fulmoon@gmail.com>
2022-07-24 21:07:37 +0900

    Style: format code, remove duplicated padding  onAir

--- lib/homework9.dart
+++ lib/homework9.dart
@@ -3,28 +3,8 @@ import 'etc/ch5_exam1.dart';
 import 'etc/ch5_exam2.dart';
 
 
-class Homework9 extends StatefulWidget {
+class Homework9 extends StatelessWidget {
   const Homework9({Key? key}) : super(key: key);
-
-  @override
-  State<Homework9> createState() => _Homework9State();
-}
-
-class _Homework9State extends State<Homework9> {
-  String data = '';
-
-  @override
-  void initState() {
-    super.initState();
-
-    initData();
-  }
-
-  Future initData() async {
-    data = await getData();
-
-    setState(() {});
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,89 +13,73 @@ class _Homework9State extends State<Homework9> {
         title: const Text('FutureBuilder 예제'),
       ), //AppBar
       body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Center(
-            child: Column(
-              children: [
-                data.isEmpty
-                ? const CircularProgressIndicator()
-                : Text(
-                  data,
-                  style: const TextStyle(fontSize: 20),
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: FutureBuilder<List<Map<String, dynamic>>>(
-                      future: getList(),
-                      builder: (context, snapshot){
-                        if (snapshot.hasError){
-                          return const Center(
-                            child: Text('에러가 발생했습니다.'),
-                          );
-                        }
+        child: Expanded(
+          child: Column(
+            children: [
+              FutureBuilder<String>(
+                future: getData(),
+                builder: (context, snapshot){
+                  if (snapshot.hasError){
+                    return const Center(
+                      child: Text('에러가 발생했습니다.'),
+                    );
+                  }
 
-                        if (snapshot.connectionState == ConnectionState.waiting){
-                          const CircularProgressIndicator();
-                        }
+                  if (!snapshot.hasData){
+                    return const Center(
+                      child: Text('데이터가 없습니다'),
+                    );
+                  }
 
-                        if (!snapshot.hasData){
-                          return const Center(
-                            child: Text('데이터가 없습니다'),
-                          );
-                        }
+                  final String data = snapshot.data!;
 
-                        final List<Map<String, dynamic>> users = snapshot.data!;
-
-                        return ListView.builder(
-                          itemBuilder: (BuildContext context, int index) {
-                            if (users.isEmpty) {
-                              const CircularProgressIndicator();
-                            }
-                            Map<String, dynamic> user = users[index];
-                            return Padding(
-                              padding: const EdgeInsets.all(16.0),
-                              child: Text(
-                                user['title'],
-                                style: const TextStyle(
-                                  fontSize: 20,
-                                ),
-                              ),
-                            );
-                          }
-                        );
-                      },
+                  return SizedBox(
+                    child: Text(
+                      data,
+                      style: const TextStyle(fontSize: 20),
                     ),
-                  )
-                ),
-              ],
-            ),
-          ),
+                  );
+                }),
+
+              FutureBuilder<List<Map<String, dynamic>>>(
+                future: getList(),
+                builder: (context, snapshot){
+
+                  if (!snapshot.hasData){
+                    return const Center(
+                      //child: Text('데이터가 없습니다'),
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+
+                  final List<Map<String, dynamic>> users = snapshot.data!;
+
+                  return ListView.builder(
+                    itemBuilder: (BuildContext context, int index) {
+                      Map<String, dynamic> user = users[index];
+
+                      return Text(
+                        user['title'],
+                        style: const TextStyle(
+                          fontSize: 20,
+                        ),
+                      );
+                    }
+                  );
+                },
+              ),
+            ],
+          )
         ),
       ),
     );
   }
 
   Future<String> getData() async {
-    await Future.delayed(const Duration(seconds: 3));
+    await Future.delayed(const Duration(seconds: 1));
 
     return exam1.toString();
   }
-/*
-  Future<List<Exam2Title>> getList() async {
-    await Future.delayed(const Duration(seconds: 3));
-
-    String jsonString = exam2;
-
-    Exam2Title exam21 = jsonDecode(jsonString);
-
-    Iterable temp = [exam21];
-
-    List<Exam2Title> result = temp.map((e) => Exam2Title.fromJson(e)).toList();
-
-    return result;
-  }*/
 
   Future<List<Map<String, dynamic>>> getList() async {
     await Future.delayed(const Duration(seconds: 3));
